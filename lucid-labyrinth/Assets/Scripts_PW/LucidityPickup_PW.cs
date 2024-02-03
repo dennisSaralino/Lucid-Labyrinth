f17b0639cc57077436a2404ed6fba2ea7852c5a9
pwest22 <pwest7@kent.edu>
2024-02-03 15:01:31 -0500

    Created lucidity pickup prototype

---
+++ lucid-labyrinth/Assets/Scripts_PW/LucidityPickup_PW.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LucidityPickup_PW : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            float spawnPosX = UnityEngine.Random.Range(-9f, 9f);
+            float spawnPosZ = UnityEngine.Random.Range(-9f, 9f);
+            this.transform.position = new Vector3(spawnPosX, 1.0f, spawnPosZ);
+            //this.gameObject.SetActive(false);
+        }
+    }
+}
