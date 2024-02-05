04328e78273a0e7e4fbc83ac82b815733637dfb9
dennisSaralino <djsaralino@gmail.com>
2024-02-05 16:09:05 -0500

    initial environment controller

--- lucid-labyrinth/Assets/Scripts_DS/EnvironmentController.cs
+++ lucid-labyrinth/Assets/Scripts_DS/EnvironmentController.cs
@@ -1,31 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EnvironmentController : MonoBehaviour
 {
     public Slider lucidityBar;
-
-    /*
-    private void ChangeState()
+    private enum state
     {
-        switch(lucidityBar.value)
-        {
-            case 0:
+        Nightmare,
+        Neutral,
+        Lucid
+    }
+    private state currentState;
+    
 
+    private void Report()
+    {
+        switch(currentState)
+        {
+            case state.Nightmare:
+                Debug.Log("In nightmare mode"); break;
+
+            case state.Neutral:
+                Debug.Log("In neutral mode"); break;
+
+            case state.Lucid:
+                Debug.Log("In lucid mode"); break;
         }
     }
-    */
-}
 
-public class State
-{
-    public virtual void ReportChange(EnvironmentController env) { }
-}
-
-public class Nightmare : State
-{
-    public override void ReportChange(EnvironmentController env) { }
+    private void FixedUpdate()
+    {
+        if (lucidityBar.value <= 25 && lucidityBar.value > 0)
+        {
+            currentState = state.Nightmare;
+        }
+        else if (lucidityBar.value > 25 && lucidityBar.value <= 75)
+        {
+            currentState = state.Neutral;
+        }
+        else if (lucidityBar.value > 75)
+        {
+            currentState = state.Lucid;
+        }
+        Report();
+    }
 }
