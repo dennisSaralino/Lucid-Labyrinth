15ca7c3a4fcfb885ec8e9033e40042932fc7f466
dennisSaralino <djsaralino@gmail.com>
2024-02-14 13:10:38 -0500

    removed Update() in playerController

--- lucid-labyrinth/Assets/Scripts_JS/PlayerController.cs
+++ lucid-labyrinth/Assets/Scripts_JS/PlayerController.cs
@@ -99,36 +99,6 @@ public class PlayerController : MonoBehaviour
         mainCam.transform.rotation = Quaternion.Euler(camQuat);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey("left"))
-        {
-            transform.Rotate(0.0f, -2.0f, 0.0f);
-        }
-        if (Input.GetKey("right"))
-        {
-            transform.Rotate(0.0f, 2.0f, 0.0f);
-        }
-        //if (Input.GetKey("w"))
-        //{
-        //    transform.position = transform.position + (speedScalar * transform.forward);
-        //}
-        //if (Input.GetKey("a"))
-        //{
-        //    transform.position += (speedScalar * -transform.right);
-        //}
-        //if (Input.GetKey("s"))
-        //{
-        //    transform.position += (speedScalar * -transform.forward);
-        //}
-        //if (Input.GetKey("d"))
-        //{
-        //    transform.position += (speedScalar * transform.right);
-        //}
-
-    }
-
     float reduceNum(float x)
     {
         if (-1 <= x && x <= 1) {
