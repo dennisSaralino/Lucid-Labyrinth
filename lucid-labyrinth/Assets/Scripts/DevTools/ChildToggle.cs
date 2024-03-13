11395132301fd50b618e1e517eb6580cedc80df6
Coggs92 <82614562+Coggs92@users.noreply.github.com>
2024-03-12 23:34:53 -0400

    Asset Construction pt2
    Prepped for presentation, Start and End tiles, and thin wall preview

---
+++ lucid-labyrinth/Assets/Scripts/DevTools/ChildToggle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildToggle : MonoBehaviour
+{
+    //public bool ObjChildren[];
+    public List<GameObject> objChildren;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
