820a3ee141a1da63b83ec5136c85e79bfde22ed6
dennisSaralino <djsaralino@gmail.com>
2024-04-12 13:53:47 -0400

    working on volume option

--- lucid-labyrinth/Assets/Scripts/GameControlScripts/LevelLoader.cs
+++ lucid-labyrinth/Assets/Scripts/GameControlScripts/LevelLoader.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
+    public void Awake()
+    {
+        if (PlayerPrefs.HasKey("volume"))
+        {
+            audioMixer.SetFloat("volume", PlayerPrefs.GetFloat("volume"));
+        }
+    }
     public void LoadLevel(int sceneIndex)
     {
         SceneManager.LoadSceneAsync(sceneIndex);
